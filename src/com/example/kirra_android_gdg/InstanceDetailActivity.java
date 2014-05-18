7d69555a77d665cf15a398379bc5ac6fe22871d2
Rafael Chaves <rafael@abstratt.com>
2014-05-18 00:48:16 -0300

    improved layout, fixed label casing (no longer camel case)

--- src/com/example/kirra_android_gdg/InstanceDetailActivity.java
+++ src/com/example/kirra_android_gdg/InstanceDetailActivity.java
@@ -1,19 +1,19 @@
 package com.example.kirra_android_gdg;
 
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.RelativeLayout.LayoutParams;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
-import com.abstratt.kirra.Entity;
 import com.abstratt.kirra.Instance;
 
 public class InstanceDetailActivity extends Activity {
@@ -25,6 +25,7 @@ public class InstanceDetailActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_instance_detail);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		if (savedInstanceState != null) {
 			Instance serializable = (Instance) savedInstanceState
@@ -45,40 +46,34 @@ public class InstanceDetailActivity extends Activity {
 		Integer lastId = null;
 		for (Entry<String, Object> entry : values) {
 			TextView fieldName = new TextView(this);
-			fieldName.setText(entry.getKey());
-			LayoutParams params = new LayoutParams(
+			fieldName.setText(StringUtils.join(StringUtils.splitByCharacterTypeCamelCase(StringUtils.capitalize(entry.getKey())), " ") + ":");
+			LayoutParams labelParams = new LayoutParams(
 					RelativeLayout.LayoutParams.WRAP_CONTENT,
 					RelativeLayout.LayoutParams.WRAP_CONTENT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-			params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+			labelParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
 
-			if (lastId != null) {
-				params.addRule(RelativeLayout.BELOW, lastId);
-			}
+			Integer belowAnchor = lastId;
 			lastId = View.generateViewId();
 			fieldName.setId(lastId);
 
-			fieldName.setLayoutParams(params);
-			layout.addView(fieldName);
-
-			if (entry.getValue() != null) {
-				TextView value = new TextView(this);
-				value.setText(entry.getValue().toString());
-				LayoutParams valueParams = new LayoutParams(
-						RelativeLayout.LayoutParams.WRAP_CONTENT,
-						RelativeLayout.LayoutParams.WRAP_CONTENT);
-				value.setLayoutParams(valueParams);
-				valueParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-				valueParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-				valueParams.addRule(RelativeLayout.RIGHT_OF, fieldName.getId());
-				if (lastId != null) {
-					valueParams.addRule(RelativeLayout.BELOW, lastId);
-
-				}
-				lastId = View.generateViewId();
-				value.setId(lastId);
-				layout.addView(value);
+			TextView value = new TextView(this);
+			value.setText(entry.getValue() == null ? "" : entry.getValue().toString());
+			LayoutParams valueParams = new LayoutParams(
+					RelativeLayout.LayoutParams.WRAP_CONTENT,
+					RelativeLayout.LayoutParams.WRAP_CONTENT);
+			valueParams.leftMargin = 5;
+			valueParams.addRule(RelativeLayout.RIGHT_OF, lastId);
+			if (belowAnchor != null) {
+				labelParams.addRule(RelativeLayout.BELOW, belowAnchor);
+				valueParams.addRule(RelativeLayout.BELOW, belowAnchor);
 			}
+			
+			labelParams.topMargin = 8;
+			valueParams.topMargin = 8;
+			fieldName.setLayoutParams(labelParams);
+			layout.addView(fieldName);
+			value.setLayoutParams(valueParams);
+			layout.addView(value);
 		}
 	}
 }
